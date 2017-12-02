80338d6ea4622c441cb703450cafe7f994620879
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-12-02 20:31:36 +0900

    carrierwaveとmini_magickの導入

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,4 +2,20 @@ class LessonsController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+      if @lesson.save
+        redirect_to lessons_path
+      end
+  end
+
+  private
+
+   def lesson_params
+     params.require(:lesson).permit(:title,:body,:image).merge(user_id: current_user.id)
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :image, ImagesUploader
   validates :name, presence: true
   validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
